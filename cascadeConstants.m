adc754e09f61da27b6fd64691bed8533169e2194
Daniel Terry <dsterry7@gmail.com>
2017-04-19 20:14:46 -0400

    cascadeConstants: update version to 3.4.0

--- cascadeConstants.m
+++ cascadeConstants.m
@@ -17,7 +17,7 @@ end
 constants.tstamp = now();
 
 % Version info displayed in title bars
-constants.version = '3.3.0';
+constants.version = '3.4.0';
 constants.software = ['Cornell SPARTAN ' constants.version];
 
 
