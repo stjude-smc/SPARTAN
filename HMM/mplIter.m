37e04dccbe898454f88ec9029af53daf64822802
Daniel Terry <rrowv1@gmail.com>
2018-08-22 17:10:01 -0400

    MPL: correctly handle degenerate states, use sigma instead of sigma^2,   simplify code, tune convergence criteria, enable parallel calculation   of finite differences, option to modify input model during execution   to show intermediate results, add waitbar.

--- HMM/mplIter.m
+++ HMM/mplIter.m
@@ -1,89 +1,76 @@
-function [LL,dLL] = mplIter(data, dt, p0, classidx, rateMask, params)
+function LL = mplIter(data, dt, p0, classidx, rateMask, params)
 % Maximum Point Likelihood algorithm (MPL)
 %
-%   [LL,dLL] = mplIter(DATA, DT, RATES, p0, CLASSIDX, PARAMS)
+%   LL = mplIter(DATA, DT, p0, CLASSIDX, RATEMASK, PARAMS)
 %   Runs one iteration of the Maximum Point Likelihood algorithm (MPL).
-%   DATA are experimental observations (one molecule trace per row).
+%   DATA is a matrix of FRET observations (one molecule trace per row).
 %   DT is the experimental sampling interval of the data in seconds.
 %   P0 is a vector of initial state probabilities.
 %   CLASSIDX gives the class number of each state.
-%
+%   RATEMASK is true for rates that are to be varied in optimization, with
+%     the same size as the full rate matrix (Q).
 %   PARAMS is a vector of all optimizable parameters arranged as follows:
 %     [mu1 mu2 ... stdev1 stdev2 ... k12 k13 .. k21 k23 .. k31 k32 .. ...].
+%   The rate constants in this list are obtained as: Q(rateMask)'.
 %
-%   LL is the log likelihood for current parameters: log[ P(data|model) ].
-%   dLL is the gradient of the LL function w/r/t each element in PARAMS.
-%   Both values are negated to facilitate use with minimizers (e.g., fmincon).
+%   LL is the -log(likelihood) for current parameters: -log[ P(data|model) ]
+%   This allows for use with minimizers (e.g., fmincon).
 %  
 %   See Qin et al (2000) Biophys J 79, pg. 1915-1927 for algorithm details.
 %
 %   See also: mplOptimize, bwOptimize, milOptimize, batchKinetics.
-%
 
 %   Copyright 2018 Cornell University All Rights Reserved.
 
-% disp(params);
 
+narginchk(6,6);
+nargoutchk(0,1);
 
 
 %% Process input arguments
-narginchk(6,6);
-nargoutchk(0,2);
+nStates = numel(classidx);
+nClass  = max(classidx);
 
-nStates = numel(p0);
+% Make sure vectors are the correct orientation.
 p0 = reshape(p0, 1, nStates);
 if isvector(data)
     data = reshape(data, 1, numel(data));
 end
-nTraces = size(data,1);
 
 % Unpack parameters from fminunc input vector
-mu     = params( 1:nStates );
-sigma2 = params( nStates   + (1:nStates) );
-sigma2(sigma2<1e-6) = 1e-7;  %steer optimizer from zero or negative sigma.
-sigma = sqrt(sigma2);
+mu    = params( 1:nClass );
+sigma = params( nClass + (1:nClass) );
 
 Q = zeros(nStates);
-Q(rateMask) = params( 2*nStates + 1:end );
+Q(rateMask) = params( 2*nClass + 1:end );
 I = logical(eye(nStates));
 Q(I) = -sum(Q,2);
 
-
-% Calculate transition probability matrix (A) using spectral matrices.
-% See pg. 615 of 1995 book "Single Channel Recording". See eq. 6 in Qin 2000.
-% expm(Q*dt) would be simpler, but we need the spectral matrices anyway.
-[righteig,eigval] = eig(Q);
-eigval = diag(eigval);
-transitionProb = zeros(nStates);
-spectralMatrix = cell(nStates,1);
-
-for s=1:nStates
-    lefteig = righteig^-1;
-    spectralMatrix{s} = righteig(:,s) * lefteig(s,:);
-    transitionProb = transitionProb  +  exp(eigval(s)*dt) * spectralMatrix{s};
-end
+% Calculate discrete-time transition probabilities (A) from rate matrix
+transitionProb = expm( Q*dt );
 
 
 %% Calculate log likelihood and partial derivatives for each trace.
 LL = 0;
 
-for n=1:nTraces
+for n=1:size(data,1)
     % Remove frames after donor photobleaching (which are precisely zero).
     trace = data(n,:);
     bleachFrame = find( trace==0, 1, 'first' );
     trace = trace(1:bleachFrame-1);
     nFrames = numel(trace);
-    if nFrames<10, continue; end  %skip extremely short traces
+    if nFrames<5, continue; end  %skip extremely short traces
     
     % Calculate emmission probabilities at each timepoint
     observProb = zeros(nFrames, nStates);
     for i=1:nStates
-        observProb(:,i) = exp(-0.5 * ((trace - mu(i))./sigma(i)).^2) ./ (sqrt(2*pi) .* sigma(i));
+        c = classidx(i);
+        observProb(:,i) = exp(-0.5 * ((trace - mu(c))./sigma(c)).^2) ./ (sqrt(2*pi) .* sigma(c));
     end
-    observProb = observProb+eps;  %prevent rows of all zeros
+    observProb( all(observProb==0,2), : ) = eps;  %prevent rows of all zeros
     
     % Get partial probabilities using the forward-backward algorithm
-    [LLtrace,~] = BWtransition( p0, transitionProb, observProb );
+    [LLtrace,~] = BWtransition( p0, transitionProb, observProb/100 );
     LL = LL+LLtrace;
 
 end %for each trace
@@ -91,7 +78,6 @@ end %for each trace
 % Return opposite of LL and dLL since to convert maximizing LL into minimizing
 % -LL for use with fminunc/fmincon.
 LL = -LL;
-
 
 
 end  %function mplIter
--- HMM/mplOptimize.m
+++ HMM/mplOptimize.m
@@ -30,6 +30,8 @@ narginchk(3,4);
 nargoutchk(1,3);
 
 nStates = model.nStates;
+nClass  = model.nClasses;
+
 I = logical(eye(nStates));
 rateMask = ~I & model.rates~=0;
 nRates = sum(rateMask(:));
@@ -37,68 +39,47 @@ dt = dt/1000;  %convert to seconds/frame
 
 
 % Define default optional arguments, mostly taken from fmincon
-options.maxIter  = 200;
-options.convLL   = 10^-6;   %OptimalityTolerance in fmincon (sort of)
-options.convGrad = 10^-8;  %StepTolerance in fmincon
-options.verbose  = true;
+options = struct('maxIter',200,  'convLL',10^-6, 'convGrad',10^-6, ...
+                 'verbose',true, 'updateModel',false);
 if nargin>=4
     options = mergestruct(options, optionsInput);
 end
 if isfield(options,'exclude') && any(options.exclude)
-    error('Excluding traces not supported by MPL yet');
+    warning('Excluding traces not supported by MPL yet');
 end
 
 % Construct options for fmincon.
-fminopt = optimoptions('fmincon');
+fminopt = optimoptions('fmincon', 'UseParallel',cascadeConstants('enable_parfor') );
 if options.verbose
     fminopt.Display='iter';
     fminopt.OutputFcn = @outfun;
 end
+fminopt.MaxIter = options.maxIter;
+fminopt.TolX    = options.convGrad;
+fminopt.TolFun  = options.convLL;
 
-try
-    % Legacy version for MATLAB R2015a and before
-    fminopt.MaxIter = options.maxIter;
-    fminopt.TolX    = options.convGrad;
-    fminopt.TolFun  = options.convLL;
-%     fminopt.GradObj = 'on';
-catch
-    fminopt.MaxIterations            = options.maxIter;
-    fminopt.StepTolerance            = options.convGrad;
-    fminopt.OptimalityTolerance      = options.convLL;
-%     fminopt.SpecifyObjectiveGradient = true;
-end
+% Run fmincon optimizer, with loose contraints to aid convergence.
+optFun = @(x)mplIter(fret, dt, model.p0, model.class, rateMask, x);
+x0 = [ model.mu(:)' model.sigma(:)' model.rates(rateMask)' ];
 
-% Define optimization function and initial parameter values.
-% NOTE: mplIter optimizes the variance (sigma^2).
-optFun = @(x)mplIter(fret(1:100,:), dt, model.p0, model.class, rateMask, x);
-x0 = [ model.mu(:)'  model.sigma(:)'.^2  model.rates(rateMask)'  ];
-
-% Constrained version.
-% NOTE: fmincon doesn't like x0=lb or x0=ub in any parameter and will 'fix'
-% things in ways that can break the bounds, so I added -eps to the minimum rate
-% in hopes that the actual minimum is 0.
-lb = [ -0.5*ones(1,nStates)  0.01^2*ones(1,nStates)   -eps*ones(1,nRates) ];
-ub = [  1.5*ones(1,nStates)  0.15^2*ones(1,nStates)  10/dt*ones(1,nRates) ];
+lb = [ -0.3*ones(1,nClass)  0.01*ones(1,nClass)     zeros(1,nRates) ];
+ub = [  1.2*ones(1,nClass)  0.12*ones(1,nClass) 3/dt*ones(1,nRates) ];
 [optParam,LL] = fmincon( optFun, x0, [],[],[],[],lb,ub,[],fminopt );
 
 % Save results
 optModel = copy(model);  %do not modify model in place
-optModel.mu    = optParam(1:nStates);
-optModel.sigma = sqrt( optParam(nStates + (1:nStates)) );
+optModel.mu    = optParam(1:nClass);
+optModel.sigma = optParam(nClass + (1:nClass));
 optModel.rates(:) = 0;
-optModel.rates(rateMask) = optParam(2*nStates + 1:end);
-
-% Use forward-backward algorithm to estimate initial probabilities.
-% We don't directly optimize these parameters because the gradients are
-% constant.
-% [~,~,~,gamma] = BWtransition();
+optModel.rates(rateMask) = optParam(2*nClass + 1:end);
 
 % Idealize traces if requested.
-idl = idealize( fret, [to_col(optModel.mu) to_col(optModel.sigma)], optModel.p0, optModel.calcA(dt) );
-
-
-end %function mplOptimize
-
+% FIXME: idealize should properly handle degenerate states
+imu    = optModel.mu( optModel.class );
+isigma = optModel.sigma( optModel.class );
+idl = idealize( fret, [to_col(imu) to_col(isigma)], optModel.p0, optModel.calcA(dt) );
+classes = [0; to_col(model.class)];
+idl = reshape( classes(idl+1), size(idl) );
 
 
 
@@ -109,42 +90,67 @@ function stop = outfun(x,optimValues,state)
 
 persistent X;
 persistent dX;
+persistent wbh;
 stop=false;  %if true, optimizer terminates early.
+itr = optimValues.iteration;
 
 switch state
     case 'init'
         X  = zeros( 1000, numel(x) );
         dX = zeros( 1000, numel(x) );
+        wbh = waitbar(0,'Running MPL...');
           
     case 'iter'
         % Keep track of parameter values at each iteration
-        X(optimValues.iteration+1,:)  = x;
-        dX(optimValues.iteration+1,:) = optimValues.gradient;
+        X(itr+1,:)  = x;
+        dX(itr+1,:) = optimValues.gradient;
+        
+        if options.updateModel
+            model.mu    = x( 1:nClass );
+            model.sigma = x( nClass + (1:nClass) );
+            model.rates(rateMask) = x( 2*nClass + 1:end );
+            drawnow;
+        end
+        
+        progress = max( itr/options.maxIter, log10(optimValues.stepsize)/log10(options.convLL) );
+        if ~ishandle(wbh) || ~isvalid(wbh)
+            stop=true; %user closed waitbar
+            return;
+        end
+        if itr>1,  waitbar( max(0,min(1,progress)), wbh );  end
           
     case 'done'
+        if ishandle(wbh), close(wbh); end
+        if ~options.verbose, return; end
+        
         % Once optimizer completes, plot how parameters change over iterations
-        X  = X(1:optimValues.iteration,:);
-        dX = dX(1:optimValues.iteration,:);
+        X  = X(1:itr,:);
+        dX = dX(1:itr,:);
         
         figure;
         for i=1:numel(x)
             ax(1,i) = subplot(2,numel(x),i);
-            plot(1:optimValues.iteration, X(:,i)', 'k.-', 'MarkerFaceColor',[1 0 1]);
+            plot(1:itr, X(:,i)', 'k.-', 'MarkerFaceColor',[1 0 1]);
             if i==1
                 ylabel('Param. Value');
             end
             %title('mu2');
             
             ax(2,i) = subplot(2,numel(x),numel(x)+i);
-            plot(1:optimValues.iteration, dX(:,i)', 'k.-', 'MarkerFaceColor',[1 0 1]);
+            plot(1:itr, dX(:,i)', 'k.-', 'MarkerFaceColor',[1 0 1]);
             if i==1
                 xlabel('Iteration');
                 ylabel('Gradient');
             end
         end
         linkaxes(ax(:), 'x');
-        xlim(ax(1), [1,optimValues.iteration]);
+        xlim(ax(1), [1,itr+1]);
+        disp(X);
 end
 
-
 end %function outfun
+
+
+
+
+end %function mplOptimize
