86ea4a1bb20e8470a8fca362f44626ec5edbb5db
Roman Kiselev <roman.kiselev@stjude.org>
2024-11-27 14:56:57 -0600

    `selectPrintedSpots` now based on Canny edges and circle fitting This is more complex method, and it required multiple files. I created a folder `demux` and put them there. The old `selectPrintedSpots` have been renamed to `selectPrintedSpots-gaussian`.

---
+++ demux/find_spot_edges.m
@@ -0,0 +1,59 @@
+function pixel_coords = find_spot_edges(traces, ax)
+    % Scale factors
+    s1 = 2;
+    s2 = 4;
+
+    if ~isfield(traces, 'traceMetadata') || ~isfield(traces, 'fileMetadata')
+        error('Invalid input: `traces` must contain `traceMetadata` and `fileMetadata` fields.');
+    end
+
+    x = [traces.traceMetadata.donor_x];
+    y = [traces.traceMetadata.donor_y];
+
+    % Get dimensions from file metadata
+    nX = traces.fileMetadata.nX;
+    nY = traces.fileMetadata.nY;
+
+    % Initialize a binary image with zeros
+    location_img = zeros(nX, nY);
+
+    % Set ones at the specified x and y locations
+    for i = 1:length(x)
+        location_img(x(i), y(i)) = 1;
+    end
+
+    % Mask and resize
+    msk = make_spot_mask(location_img, 160, 150);
+    thumbnail = imresize(location_img .* msk, 1/s1, 'bilinear');
+
+    % Dilation to increase signal in circles
+    thumbnail = imdilate(thumbnail, strel('disk', round(4))); 
+
+    % Downscale once again
+    thumbnail = imresize(thumbnail, 1/s2, 'bilinear');
+    thumbnail = thumbnail ./ max(thumbnail(:));
+
+    % Apply Canny edge detection
+    edges = edge(thumbnail, 'Canny', [0.1, 0.4], 6);
+
+    % Mask canny edges
+    msk = make_spot_mask(edges, 160/s1/s2, 140/s1/s2) - make_spot_mask(edges, 170/s1/s2, 70/s1/s2);
+    edges = edges .* msk;
+
+    % Assuming `edges` is your binary image
+    [row_coords, col_coords] = find(edges);
+
+    % Combine the row and column coordinates into a single array (optional)
+    pixel_coords = [row_coords, col_coords] * s1 * s2;
+
+    
+    % Optional plotting if ax is provided
+    if nargin > 1 && ~isempty(ax) && isgraphics(ax, 'axes')
+        hold(ax, 'on'); % Ensure we can plot multiple elements
+
+        % Plot detected edges
+        imshow(imdilate(location_img, strel('disk', round(4))));
+        plot(ax, pixel_coords(:, 2), pixel_coords(:, 1), 'y.');
+        axis(ax, 'equal');
+    end
+end
---
+++ demux/fit_circles.m
@@ -0,0 +1,86 @@
+function circles = fit_circles(traces, edge_coords, ax)
+    % Find the circle centers from edge coordinates and optionally plot them.
+    % Input:
+    %   traces       - Struct containing image metadata (e.g., dimensions)
+    %   edge_coords  - Nx2 array of edge pixel coordinates
+    %   ax           - Optional axes object for plotting
+    % Output:
+    %   circle_centers - 4x2 array of [cx, cy] for the circles in each quadrant
+
+    % Get image center
+    nX = traces.fileMetadata.nX;
+    nY = traces.fileMetadata.nY;
+    cx_img = nX / 2;
+    cy_img = nY / 2;
+
+    % Initialize quadrants
+    tl = []; % Top-left quadrant
+    tr = []; % Top-right quadrant
+    bl = []; % Bottom-left quadrant
+    br = []; % Bottom-right quadrant
+
+    % Split edge_coords into quadrants
+    for i = 1:size(edge_coords, 1)
+        x = edge_coords(i, 1);
+        y = edge_coords(i, 2);
+        if x <= cx_img && y <= cy_img
+            tl = [tl; x, y]; % Top-left
+        elseif x <= cx_img && y > cy_img
+            tr = [tr; x, y]; % Top-right
+        elseif x > cx_img && y <= cy_img
+            bl = [bl; x, y]; % Bottom-left
+        else
+            br = [br; x, y]; % Bottom-right
+        end
+    end
+
+    % Fit a circle for each quadrant
+    circles = zeros(4, 3); % To store [cx, cy, r] for each circle
+    quadrants = {tl, tr, bl, br};
+    for q = 1:4
+        edges = quadrants{q};
+        [cx, cy, r] = fit_circle(edges);
+        circles(q, :) = [cx, cy, r];
+
+        % Optional plotting if ax is provided
+        if nargin > 2 && ~isempty(ax) && isgraphics(ax, 'axes')
+            % Calculate the radius based on the average distance from center to edge points
+            x = edges(:, 1);
+            y = edges(:, 2);
+            %r = mean(sqrt((x - cx).^2 + (y - cy).^2));
+
+            % Plot the circular patch
+            rectangle(ax, 'Position', [cy - r, cx - r, 2*r, 2*r], ...
+                      'Curvature', [1, 1], 'EdgeColor', 'r', 'LineWidth', 0.5);
+
+            % Plot the center
+            plot(ax, cy, cx, 'g+', 'MarkerSize', 10, 'LineWidth', 2, ...
+                 'DisplayName', ['Center ', num2str(q)]);
+        end
+    end
+
+end
+
+function [cx, cy, r] = fit_circle(edges)
+    % Fit a circle to a set of edge points using least-squares
+    % Input:
+    %   edges - Nx2 array of edge coordinates [x, y]
+    % Output:
+    %   [cx, cy] - Fitted circle center
+
+    % Extract x and y coordinates
+    x = edges(:, 1);
+    y = edges(:, 2);
+
+    % Set up the linear system for least-squares
+    A = [2*x, 2*y, ones(size(x))];
+    b = x.^2 + y.^2;
+
+    % Solve the normal equations A * [cx; cy; c] = b
+    params = A \ b;
+
+    % Extract circle center
+    cx = params(1);
+    cy = params(2);
+    r = sqrt(params(3) + cx^2 + cy^2);
+end
