b53fb1887e2d5a1e8f3055edf0b2fddbca95ccbb
Daniel Terry <daniel.terry@stjude.org>
2025-05-23 14:41:27 -0500

    Use Arial 9, normalized units for controls, and pixel units for figure, for all GUIDE UIs.

--- misc/calculateR0.m
+++ misc/calculateR0.m
@@ -22,10 +22,10 @@ function varargout = calculateR0(varargin)
 
 % Edit the above text to modify the response to help calculateR0
 
-% Last Modified by GUIDE v2.5 11-Jan-2024 15:10:33
+% Last Modified by GUIDE v2.5 23-May-2025 14:02:26
 
 % Begin initialization code - DO NOT EDIT
-gui_Singleton = 0;
+gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
                    'gui_OpeningFcn', @calculateR0_OpeningFcn, ...
